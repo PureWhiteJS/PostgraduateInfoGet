f0f74b4f8d4da028497f13243558f51895c48c4a
PureWhiteJS <2228965481@qq.com>
2022-10-07 21:01:29 +0800

    最初设想的功能全部实现！ 能够正常运行收集信息并生成execl文件，分开测试一切正常，尝试完整运行2次没有出现错误

--- PostgraduateInfoGet.py
+++ PostgraduateInfoGet.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 # 导入Select类
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.edge.service import Service
 # 新建修改execl表格
 import openpyxl
 
@@ -37,95 +38,116 @@ def initExeclSheet(execlSheet):
     execlSheet['M1'] = '备注'
 
 
-def addExeclSheet(baseData, subjectData, other=""):
+def addExeclSheet(execlSheet, baseData, subjectData, other=""):
     global addExeclSheet_counter
     addExeclSheet_counter = addExeclSheet_counter + 1
-    execlSheet['A' + addExeclSheet_counter] = baseData[0]
-    execlSheet['B' + addExeclSheet_counter] = baseData[2]
-    execlSheet['C' + addExeclSheet_counter] = baseData[3]
-    execlSheet['D' + addExeclSheet_counter] = baseData[5]
-    execlSheet['E' + addExeclSheet_counter] = baseData[4]
-    execlSheet['F' + addExeclSheet_counter] = baseData[1]
-    execlSheet['G' + addExeclSheet_counter] = subjectData[6]
-    execlSheet['H' + addExeclSheet_counter] = subjectData[7]
-    execlSheet['I' + addExeclSheet_counter] = subjectData[0]
-    execlSheet['J' + addExeclSheet_counter] = subjectData[1]
-    execlSheet['K' + addExeclSheet_counter] = subjectData[2]
-    execlSheet['L' + addExeclSheet_counter] = subjectData[3]
-    execlSheet['M' + addExeclSheet_counter] = other
+    strCounter = str(addExeclSheet_counter)
+    execlSheet['A' + strCounter] = baseData[0]
+    execlSheet['B' + strCounter] = baseData[2]
+    execlSheet['C' + strCounter] = baseData[3]
+    execlSheet['D' + strCounter] = baseData[5]
+    execlSheet['E' + strCounter] = baseData[4]
+    execlSheet['F' + strCounter] = baseData[1]
+    execlSheet['G' + strCounter] = baseData[6]
+    execlSheet['H' + strCounter] = baseData[7]
+    execlSheet['I' + strCounter] = subjectData[0]
+    execlSheet['J' + strCounter] = subjectData[1]
+    execlSheet['K' + strCounter] = subjectData[2]
+    execlSheet['L' + strCounter] = subjectData[3]
+    execlSheet['M' + strCounter] = other
 
 
 if __name__ == '__main__':
     # 确定driver位置
-    # driver = webdriver.Chrome('F:\Softwares\Library\chromedriver.exe')
     addExeclSheet_counter = 1
-    driver = webdriver.Edge('D:\pythonabout\pythonProject\PostgraduateInfoGet\msedgedriver.exe')
-    # driver.implicitly_wait(3)  # 设置等待3秒后打开目标网页
+    s = Service("D:\pythonabout\pythonProject\PostgraduateInfoGet\msedgedriver.exe")
+    driver = webdriver.Edge(service=s)
     url = "https://yz.chsi.com.cn/zsml/zyfx_search.jsp"
     # 新建表格文件
     execlFile = openpyxl.Workbook()
     # 获取sheet
     execlSheet = execlFile.active
     initExeclSheet(execlSheet)
-    execlFile.save("test.xlsx")
+    execlFile.save('D:\Files\Temp\\test.xlsx')
     # 打开网页
     driver.get(url)
     # 门类选择
     sel_Class = driver.find_element(By.ID, "mldm")
     sel_ClassChange = Select(sel_Class)
-    sel_ClassChange.select_by_value("02")
+    sel_ClassChange.select_by_value("13")
 
     # 专业领域选择
     sel_Field = driver.find_element(By.ID, "yjxkdm")
     sel_FieldChange = Select(sel_Field)
-    sel_FieldChange.select_by_value("0201")
+    sel_FieldChange.select_by_value("1303")
 
     # 点击查询
     btn_Search = driver.find_element(By.NAME, "button")
     btn_Search.click()
-
-    # 获取学校列表窗口的句柄方便返回翻页
     schoolWindow = driver.current_window_handle  # 保存课程页面句柄，用于后期返回课程页面
+    while(1):
+        # 获取学校列表窗口的句柄方便返回翻页
+        schoolWindow = driver.current_window_handle  # 保存课程页面句柄，用于后期返回课程页面
+        # 获取当前页所有学校的链接
+        lay_Table = driver.find_element(By.CLASS_NAME, "ch-table")
+        linkList = lay_Table.find_elements(By.TAG_NAME, "a")
+        for link in linkList:
+            print(link.text + "   " + link.get_attribute("href"))
+        # 对链接列表的链接进行处理，进入大学界面
+        for link in linkList:
+            print(link.text + "   " + link.get_attribute("href"))
+            url = link.get_attribute("href")
+            # 在新标签页中打开网页
+            js = "window.open('" + url + "');"
+            driver.execute_script(js)
+            findWindowByUrl(driver, "dwmc")#定位到新打开的网页
+            collegeWindow = driver.current_window_handle  # 保存大学页面
+            # 切换到学院列表的table中
+            collegeTable = driver.find_element(By.TAG_NAME, "table")
+            # 获取学院的查看列表
+            collegeLinkList = collegeTable.find_elements(By.TAG_NAME, "a")
+            strcollegeLinkList = []
+            for collegeLink in collegeLinkList:
+                # 筛选出查看链接不要详细链接
+                if "java" in collegeLink.get_attribute("href"):
+                    continue
+                strcollegeLinkList.append(collegeLink.get_attribute("href"))
+            print(strcollegeLinkList)
+            # 进入学院界面
+            for collegeLink in strcollegeLinkList:
+                js = "window.open('" + collegeLink + "');"
+                driver.execute_script(js)
+                # 切换到查看的窗口
+                findWindowByUrl(driver, "id")
+                # 获取数据并存入execl表格中
+                baseData = []
+                driver.find_elements(By.CLASS_NAME, "zsml-summary")
+                for a in driver.find_elements(By.CLASS_NAME, "zsml-summary"):
+                    baseData.append(a.text)
+                print(baseData)
 
-    # 获取所有学校的链接
-    lay_Table = driver.find_element(By.CLASS_NAME, "ch-table")
-    linkList = lay_Table.find_elements(By.TAG_NAME, "a")
-    # 对链接列表的链接进行处理
-    for link in linkList:
-        print(link.text + "   " + link.get_attribute("href"))
-        url = link.get_attribute("href")
-        # 在新标签页中打开网页
-        js = "window.open('" + url + "');"
-        driver.execute_script(js)
-        findWindowByUrl(driver, "dwmc")
-        # 切换到学院列表的table中
-        collegeTable = driver.find_element(By.TAG_NAME, "table")
-        # 获取学院的查看列表
-        collegeLinkList = collegeTable.find_elements(By.TAG_NAME, "a")
-        for collegeLink in collegeLinkList:
-            # 筛选出查看链接不要详细链接
-            if ("java" in collegeLink.get_attribute("href")):
-                continue
-            print(collegeLink.get_attribute("href"))
-            # 打开查看链接
-            driver.get(collegeLink.get_attribute("href"))
-            # 切换到查看的窗口
-            findWindowByUrl(driver, "id")
-            # 获取数据school,college,subject,direction,method,politics,foreignLan,businessSec1,businessSec2
-            # data
-            baseData = []
-            driver.find_elements(By.CLASS_NAME, "zsml-summary")
-            for a in driver.find_elements(By.CLASS_NAME, "zsml-summary"):
-                baseData.append(a.text)
-            print(baseData)
-            # #获取课程所在table
-            # subjectItems = driver.find_elements(By.CLASS_NAME,"zsml-res-items")#每一行
-            # #将table的每一行分别处理
-            # for subjectList in subjectItems:
-            #
-            #     for subject in subjectList.find_elements(By.TAG_NAME,"td"):
-            #         print(subject.text)
-            #         addExeclSheet
-            #
-            # print("\n")
-        # driver.quit()
+                # 获取课程所在table，将table的每一行分别处理
+                subjectData = []
+                for subjectList in driver.find_elements(By.CLASS_NAME, "zsml-res-items"):
+                    if subjectData:
+                        print(subjectData)
+                        addExeclSheet(execlSheet, baseData, subjectData)
+                        execlFile.save('D:\Files\Temp\\test.xlsx')
+                        subjectData = []
+                    for subject in subjectList.find_elements(By.TAG_NAME, "td"):
+                        subjectData.append(subject.text)
+                if subjectData:
+                    addExeclSheet(execlSheet, baseData, subjectData)
+                    execlFile.save('D:\Files\Temp\\test.xlsx')
+                    subjectData = []
+                driver.close()
+                driver.switch_to.window(collegeWindow)
+            driver.close()
+            driver.switch_to.window(schoolWindow)
+        try:
+            next_page = driver.find_element(By.CSS_SELECTOR,"[class ='lip lip-last']")
+            next_page.click()
+        except:
+            driver.quit()
+            break
+
